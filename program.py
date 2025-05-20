6a5472e544a68a2a2c44b59babf72415271369d3
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2025-05-20 13:28:25 +0300

    init

---
+++ program.py
@@ -0,0 +1,18 @@
+def findRootsOfQuadratic(a, b, c):
+  """
+  This function finds the roots of a quadratic equation (y = ax^2 + bx + c)
+  """
+  d = b ** 2 - 4 * a * c
+  
+  if d < 0:
+    raise Exception("No real roots")
+  
+  if d == 0:
+    x1 = -b / (2 * a)
+    x2 = x1
+    return (x1, x2)
+  
+  if d > 0:
+    x1 = (-b + d ** 0.5) / (2 * a)
+    x2 = (-b - d ** 0.5) / (2 * a)
+    return (x1, x2)
---
+++ test.py
@@ -0,0 +1,3 @@
+from program import findRootsOfQuadratic
+
+[x1, x2] = findRootsOfQuadratic(1, 2, 1)
